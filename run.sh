190a5fb8c31b7957461dc658a781077c4d5e3297
NVLuat <nvluathcmut@gmail.com>
2024-05-25 21:14:20 +0700

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set environment variables for Qt5
-export CMAKE_PREFIX_PATH="/usr/local/opt/qt@5/lib/cmake"
+export CMAKE_PREFIX_PATH="/usr/local/opt/qt@5/lib/cmake:$CMAKE_PREFIX_PATH"
 export Qt5_DIR="/usr/local/opt/qt@5/lib/cmake/Qt5"
 
 # Print environment variables for debugging
@@ -11,5 +11,5 @@ echo "Qt5_DIR=$Qt5_DIR"
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DQt5_DIR=$Qt5_DIR ..
+cmake ..
 make
